52b6f2916436701a7a9c11fc886373e1f2d23657
Mohamed Wagdy <mwagdy@amazon.com>
2021-09-26 15:40:04 +0100

    Remove build files

--- lib/repository/ReadingsRepository.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class ReadingsRepository {
-    static getReadingsForBox(boxId: string, from: string, to: string): Promise<any[]>;
-    static getAggregatedReadingsForBox(boxId: string, from: string, to: string): Promise<any[]>;
-}
