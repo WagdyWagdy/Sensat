16deb82f85685f7d2cfe153516946ee5b3578874
Mohamed Wagdy <mwagdy@amazon.com>
2021-09-26 15:36:46 +0100

    Clean up

--- src/controller/ReadingsController.ts
+++ src/controller/ReadingsController.ts
@@ -1,15 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import {ReadingsRepository} from "../repository/ReadingsRepository";
 
-interface readings{
-    box_id: string,
-    sensor_id: string,
-    name: string,
-    unit: string,
-    reading: string,
-    reading_ts: string
-}
-
 const getSensorData = async (req: Request, res: Response, next: NextFunction) => {
     let box: string = req.params.box_id;
     let from_date: string = req.params.from_date;
